0157c544183c7001be8e824c47f3dedc79082713
maniek <maniek@dev.pl>
2020-07-11 12:52:10 +0200

    [change] - getForEntity in progress

--- src/main/java/pl/sda/repository/WeatherRepository.java
+++ src/main/java/pl/sda/repository/WeatherRepository.java
@@ -3,6 +3,9 @@ package pl.sda.repository;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Repository
 public class WeatherRepository {
 
@@ -11,6 +14,8 @@ public class WeatherRepository {
 
     private static final String CONNECTION_URL =
             "https://samples.openweathermap.org/data/2.5/weather?q={city}&appid={key}";
+
+    private static final String DEFAULT_KEY = "439d4b804bc8187953eb36d2a8c26a02";
 
     private RestTemplate restTemplate;
 
@@ -24,6 +29,14 @@ public class WeatherRepository {
     }
 
     public String readWeatherForCity(String city) {
+
+        Map<String, String> requestParams = Map.of(
+                "city", city,
+                "key", DEFAULT_KEY
+        );
+
+        restTemplate.getForEntity(CONNECTION_URL, String.class, requestParams);
+//        restTemplate.getForEntity(CONNECTION_URL, String.class, city, DEFAULT_KEY);
         return "";
     }
 }

87082d752fff051f5520103f9ecddb6c711b5400
maniek <maniek@dev.pl>
2020-07-11 11:42:10 +0200

    [change] - service in progress

--- src/main/java/pl/sda/repository/WeatherRepository.java
+++ src/main/java/pl/sda/repository/WeatherRepository.java
@@ -4,8 +4,12 @@ import org.springframework.web.client.RestTemplate;
 
 public class WeatherRepository {
 
-    private static final String CONNECTION_URL =
+    private static final String LONDEK_CONNECTION_URL =
             "https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=439d4b804bc8187953eb36d2a8c26a02";
+
+    private static final String CONNECTION_URL =
+            "https://samples.openweathermap.org/data/2.5/weather?q=%s&appid=%s";
+
     private RestTemplate restTemplate;
 
 
@@ -14,7 +18,7 @@ public class WeatherRepository {
     }
 
     public String readRawJson() {
-        return "";
+        return restTemplate.getForObject(LONDEK_CONNECTION_URL, String.class);
     }
 
     public String readWeatherForCity(String city) {
--- src/main/java/pl/sda/service/WeatherService.java
+++ src/main/java/pl/sda/service/WeatherService.java
@@ -1,6 +1,23 @@
 package pl.sda.service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import pl.sda.repository.WeatherRepository;
+
+@Slf4j
 public class WeatherService {
 
+    private WeatherRepository repository;
 
+    public WeatherService(WeatherRepository repository) {
+        this.repository = repository;
+    }
+
+    public String getWeatherForLondek() {
+        String jsonWeather = repository.readRawJson();
+        log.info("json weather: {}", jsonWeather);
+
+        return jsonWeather;
+    }
 }

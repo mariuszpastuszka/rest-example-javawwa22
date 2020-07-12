b4d15b71e06104ee1083421a77ca9cea1828224f
maniek <maniek@dev.pl>
2020-07-12 13:31:00 +0200

    [change] - post method in progress...

--- src/main/java/pl/sda/controller/WeatherRestController.java
+++ src/main/java/pl/sda/controller/WeatherRestController.java
@@ -1,12 +1,14 @@
 package pl.sda.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import pl.sda.domain.WeatherForecast;
 import pl.sda.service.WeatherService;
 
 @RestController
+@Slf4j
 public class WeatherRestController {
 
     private WeatherService service;
@@ -18,6 +20,7 @@ public class WeatherRestController {
 
     @GetMapping("/londek")
     public String weather1() {
+        log.info("weather for londek");
         return service.getWeatherForLondek();
     }
 
@@ -29,5 +32,22 @@ public class WeatherRestController {
     @GetMapping("/weather-forecast/{city}")
     public WeatherForecast weatherForecast(@PathVariable String city) {
         return service.getWeatherForecastForCity(city);
+    }
+
+    @PostMapping("/londek")
+    @ResponseStatus(HttpStatus.CREATED)
+    public WeatherForecast saveWeatherForecast(@RequestBody WeatherForecast objectToSave) {
+        log.debug("object to save: [{}]", objectToSave);
+        // saving object
+        return objectToSave;
+    }
+
+    @PostMapping("/londek1")
+    public ResponseEntity<WeatherForecast> saveWeatherForecast1(@RequestBody WeatherForecast objectToSave) {
+        log.debug("object to save: [{}]", objectToSave);
+        // saving object
+
+
+        return objectToSave;
     }
 }
